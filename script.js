9650772b29ad2a7a256a3460d456c1f0cd10ddb7
popsikle <am.kushal02@gmail.com>
2023-07-23 08:20:32 +0530

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//
+function createAlert(prompt) {
+    if (!confirm(`Sure to ${prompt} details?`)) return false;
+    return true;
+}
+
+function submitData() {
+    if (createAlert("submit")) {
+        const form_name = document.querySelector("[data-name]").value;
+        const form_passw = document.querySelector("[data-pass]").value;
+
+        if (form_name !== "" && form_passw !== "") {
+            alert("Thank You for the data fillup!");
+            return true;
+        }
+        alert("Name and Password are required fields");
+    }
+
+    return false;
+}
